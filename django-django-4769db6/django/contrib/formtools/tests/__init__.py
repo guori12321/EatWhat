b26a867e6573300240c6d40ea68226b7c2c038f7
guori12321 <guori12321@gmail.com>
2013-04-27 21:53:31 +0800

    set up django

---
+++ django-django-4769db6/django/contrib/formtools/tests/__init__.py
@@ -0,0 +1,2 @@
+from django.contrib.formtools.tests.tests import *
+from django.contrib.formtools.tests.wizard import *
---
+++ django-django-4769db6/django/contrib/messages/tests/__init__.py
@@ -0,0 +1,5 @@
+from django.contrib.messages.tests.test_cookie import CookieTest
+from django.contrib.messages.tests.test_fallback import FallbackTest
+from django.contrib.messages.tests.test_middleware import MiddlewareTest
+from django.contrib.messages.tests.test_session import SessionTest
+from django.contrib.messages.tests.test_mixins import SuccessMessageMixinTests
