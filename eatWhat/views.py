022972a6e35f9f356fd6051499634edaaedb8504
Yu Lingtai <yltpsd@gmail.com>
2013-04-28 21:13:26 +0800

    time ver1.0

--- eatWhat/views.py
+++ eatWhat/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 import datetime
+
 
 def hello(request):
 	return HttpResponse("EatWhat?")
@@ -9,3 +10,11 @@ def current_datetime(request):
 	html = "<html><body>It is now %s.</body></html>" % now
 	return HttoResponse(html)
 
+def hours_ahead(request, offset):
+	try:
+	offset = int(offset)
+	except ValueError:
+		raise Http404()
+	dt = datetime.datetime.now()+datetime.timedelta(hours=offset)
+	html = "<html><body>In %s hour(s),it will be %s.</body></html>" % (offset, dt)
+	return HttpResponse(html)
