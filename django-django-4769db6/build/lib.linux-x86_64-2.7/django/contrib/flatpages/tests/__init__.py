e020d5bd4688b869e8c5ed7bd6e011da60aa6d16
guori12321 <guori12321@gmail.com>
2013-04-28 12:44:53 +0800

    add LOG

---
+++ django-django-4769db6/build/lib.linux-x86_64-2.7/django/contrib/flatpages/tests/__init__.py
@@ -0,0 +1,6 @@
+from django.contrib.flatpages.tests.test_csrf import *
+from django.contrib.flatpages.tests.test_forms import *
+from django.contrib.flatpages.tests.test_models import *
+from django.contrib.flatpages.tests.test_middleware import *
+from django.contrib.flatpages.tests.test_templatetags import *
+from django.contrib.flatpages.tests.test_views import *
---
+++ django-django-4769db6/build/lib.linux-x86_64-2.7/django/contrib/sitemaps/tests/__init__.py
@@ -0,0 +1,4 @@
+from .test_flatpages import FlatpagesSitemapTests
+from .test_generic import GenericViewsSitemapTests
+from .test_http import HTTPSitemapTests
+from .test_https import HTTPSSitemapTests, HTTPSDetectionSitemapTests
